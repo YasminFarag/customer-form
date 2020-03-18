6225c4ba3655e69148afd52a10c8ca4dc16cf1a8
Yasmin <yasminefawzy22@gmail.com>
2020-03-18 23:42:26 +0100

    customer-form

--- src/App.js
+++ src/App.js
@@ -1,14 +1,54 @@
-import React from "react";
+import React, { Component } from "react";
 import "./App.css";
-import Input from "./components/Input";
+import Gender from "./components/Gender";
+import Sellect from "./components/Sellect";
 
-function App() {
-  return (
-    <div className="App">
-      <h2>Customer Form</h2>
-      <Input />
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      user: {
+        name: "",
+        gender: "",
+        mail: ""
+      },
+
+      genderOptions: ["Male", "Female", "Others"]
+    };
+  }
+
+  handleInput = e => {
+    let value = e.target.value;
+    let name = e.target.name;
+    this.setState(
+      prevState => {
+        return {
+          newUser: {
+            ...prevState.newUser,
+            [name]: value
+          }
+        };
+      },
+      () => console.log(this.state.newUser)
+    );
+  };
+
+  render() {
+    return (
+      <div className="App">
+        <h2>Customer Form</h2>
+        <Gender gender={this.state.gender} />
+        <Sellect
+          title={"Gender"}
+          name={"gender"}
+          options={this.state.genderOptions}
+          value={this.state.gender}
+          placeholder={"Select Gender"}
+          handleChange={this.handleInput}
+        />
+      </div>
+    );
+  }
 }
 
 export default App;
